3397e4fda6635627a209e2663be8d863161f4e2f
Neil Rashbrook <neil@parkwaycc.co.uk>
2011-07-24 17:30:37 +0100

    Bug 673717 Live bookmarks aren't r=IanN a=IanN

--- suite/browser/navigator.js
+++ suite/browser/navigator.js
@@ -735,6 +735,9 @@ function Startup()
 
   // initialize the session-restore service
   setTimeout(InitSessionStoreCallback, 0);
+
+  // initialize the livemark service
+  setTimeout(function() { PlacesUtils.livemarks.start(); }, 5000);
 }
 
 function UpdateNavBar()
