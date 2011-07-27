c2d2a608b7d867f63f690c6c881244a1bfcfe5b3
Jim Porter <squibblyflabbetydoo@gmail.com>
2011-07-27 17:44:48 -0500

    Bug 673049 - Forwarding an .eml message causes message compose error; r=protz, a=standard8

--- mailnews/compose/src/nsMsgCompose.cpp
+++ mailnews/compose/src/nsMsgCompose.cpp
@@ -1751,7 +1751,7 @@ nsresult nsMsgCompose::CreateMessage(const char * originalMsgURI,
       m_compFields->SetReferences(reference.get());
     }
 
-    return rv;
+    return NS_OK;
   }
 
   char *uriList = PL_strdup(originalMsgURI);
