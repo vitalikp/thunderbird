0791339cfed2607596488da46e9b603b5993ff24
Jim Porter <squibblyflabbetydoo@gmail.com>
2011-10-26 20:23:38 -0500

    Bug 682639 - can't open attachment from compose window; r=standard8, a=standard8

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -3216,7 +3216,7 @@ nsAttachmentOpener.prototype =
 
   getInterface: function(iid)
   {
-    if (iid.equals(Components.interfaces.nsIDOMWindowInternal))
+    if (iid.equals(Components.interfaces.nsIDOMWindow))
       return window;
     else
       return this.QueryInterface(iid);
