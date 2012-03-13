8b28c08c053097fb4bf9c504095d1b72e90dd84f
Mark Banner <bugzilla@standard8.plus.com>
2012-03-13 21:37:18 +0000

    Version bumps for SeaMonkey, Lightning and Thunderbird a=auroramerge CLOSED TREE

--- mail/test/mozmill/migration-from-tb2/prefs.js
+++ mail/test/mozmill/migration-from-tb2/prefs.js
@@ -12,7 +12,8 @@
 user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1273257684);
 user_pref("app.update.lastUpdateTime.background-update-timer", 1273257684);
 user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1273257684);
-user_pref("extensions.checkCompatibility.nightly", false);
+user_pref("extensions.checkCompatibility.13.0a", false);
+user_pref("extensions.checkCompatibility.13.0", false);
 user_pref("extensions.getAddons.cache.enabled", false);
 user_pref("extensions.testpilot.lastversion", "1.0");
 user_pref("extensions.autoDisableScopes", 10);
