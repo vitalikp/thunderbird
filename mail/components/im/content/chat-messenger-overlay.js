bbbd983391031bcec9499cb866365b1c2a38e6f9
Mike Conley <mconley@mozilla.com>
2012-07-23 15:49:30 -0400

    Backout adb23200a7e8 because we shouldn't have landed on relbranch.

--- mail/components/im/content/chat-messenger-overlay.js
+++ mail/components/im/content/chat-messenger-overlay.js
@@ -544,7 +544,6 @@ var chatHandler = {
         conv.conv = item.conv;
         conv.tab = item;
         conv.setAttribute("contentcontextmenu", "chatConversationContextMenu");
-        conv.setAttribute("contenttooltip", "aHTMLTooltip");
         item.convView = conv;
         document.getElementById("contextSplitter").hidden = false;
         document.getElementById("contextPane").hidden = false;
