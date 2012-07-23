4cab1415c122533fdff5759deab5ede81827cbfc
Mike Conley <mconley@mozilla.com>
2012-07-23 15:47:41 -0400

    Backout 3cc8bae7f675 because we shouldn't have landed on relbranch.

--- chat/components/src/imConversations.js
+++ chat/components/src/imConversations.js
@@ -101,6 +101,7 @@ UIConversation.prototype = {
 
     delete this._currentTargetId;
     this.notifyObservers(this, "ui-conversation-closed");
+    Services.obs.notifyObservers(this, "ui-conversation-closed", null);
     return true;
   },
 
@@ -394,7 +395,6 @@ ConversationsService.prototype = {
       delete this._uiConv[aPurpleConversation.id];
       if (contactId.value)
         delete this._uiConvByContactId[contactId.value];
-      Services.obs.notifyObservers(uiConv, "ui-conversation-closed", null);
     }
     this.forgetConversation(aPurpleConversation);
   },
