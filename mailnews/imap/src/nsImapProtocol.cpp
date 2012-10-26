178864fb49732bf96008555f25cc2ec99c388acd
Irving Reid <irving@mozilla.com>
2012-10-26 11:49:37 -0400

    Bug 803843: Don't close stream before calling DiscardNewMessage; keep track of when we're downloading the entire message r=Standard8 a=Standard8

--- mailnews/imap/src/nsImapProtocol.cpp
+++ mailnews/imap/src/nsImapProtocol.cpp
@@ -374,6 +374,7 @@ nsImapProtocol::nsImapProtocol() : nsMsgProtocol(nullptr),
   m_flagState = nullptr;
   m_fetchBodyIdList = nullptr;
   m_isGmailServer = false;
+  m_fetchingWholeMessage = false;
 
   nsCOMPtr<nsIPrefBranch> prefBranch(do_GetService(NS_PREFSERVICE_CONTRACTID));
   NS_ASSERTION(prefBranch, "FAILED to create the preference service");
@@ -3265,6 +3266,8 @@ void nsImapProtocol::FetchMsgAttribute(const nsCString &messageIds, const nsCStr
     if (NS_SUCCEEDED(rv))
        ParseIMAPandCheckForNewMail(commandString.get());
     GetServerStateParser().SetFetchingFlags(false);
+    // Always clear this flag after every fetch.
+    m_fetchingWholeMessage = false;
 }
 
 // this routine is used to fetch a message or messages, or headers for a
@@ -3299,6 +3302,7 @@ nsImapProtocol::FetchMessage(const nsCString &messageIds,
   switch (whatToFetch) {
   case kEveryThingRFC822:
     m_flagChangeCount++;
+    m_fetchingWholeMessage = true;
     if (m_trackingTime)
       AdjustChunkSize();      // we started another segment
     m_startTime = PR_Now();     // save start of download time
@@ -3345,6 +3349,7 @@ nsImapProtocol::FetchMessage(const nsCString &messageIds,
       const char *formatString = "";
       eIMAPCapabilityFlags server_capabilityFlags = GetServerStateParser().GetCapabilityFlag();
 
+      m_fetchingWholeMessage = true;
       if (server_capabilityFlags & kIMAP4rev1Capability)
       {
         // use body[].peek since rfc822.peek is not in IMAP4rev1
@@ -3537,6 +3542,8 @@ nsImapProtocol::FetchMessage(const nsCString &messageIds,
       ParseIMAPandCheckForNewMail(protocolString);
     PR_Free(protocolString);
     GetServerStateParser().SetFetchingFlags(false);
+    // Always clear this flag after every fetch.
+    m_fetchingWholeMessage = false;
     if (GetServerStateParser().LastCommandSuccessful() && CheckNeeded())
       Check();
   }
@@ -3908,7 +3915,9 @@ void nsImapProtocol::NormalMessageEndDownload()
   if (!GetServerStateParser().GetDownloadingHeaders())
   {
     int32_t updatedMessageSize = -1;
-    if (m_bytesToChannel != GetServerStateParser().SizeOfMostRecentMessage()) {
+    if (m_fetchingWholeMessage &&
+        (m_bytesToChannel != GetServerStateParser().SizeOfMostRecentMessage()))
+    {
       updatedMessageSize = m_bytesToChannel;
 #ifdef DEBUG
       nsCAutoString message("Server's RFC822.SIZE ");
