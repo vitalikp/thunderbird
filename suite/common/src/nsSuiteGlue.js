c5d1470873c1b2372a4a504263c6fe767eba77ec
Dmitry Groshev <wjaguar@users.sourceforge.net>
2014-12-24 02:12:27 +0800

    Bug 1113791 - browser.bookmarks.autoExportHTML does not work r=Ratty a=Callek CLOSED TREE

--- suite/common/src/nsSuiteGlue.js
+++ suite/common/src/nsSuiteGlue.js
@@ -884,7 +884,7 @@ SuiteGlue.prototype = {
         // potential hangs (bug 518683).  The asynchronous shutdown operations
         // will then be handled by a shutdown service (bug 435058).
         var shutdownComplete = false;
-        BookmarkHTMLUtils.exportToFile(FileUtils.getFile("BMarks", [])).then(
+        BookmarkHTMLUtils.exportToFile(Services.dirsvc.get("BMarks", Components.interfaces.nsILocalFile)).then(
           function onSuccess() {
             shutdownComplete = true;
           },
