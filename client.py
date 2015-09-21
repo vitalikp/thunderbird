e38afcc6a2b61053621feb7e8d5c0868f45e58ae
Mark Banner <standard8@mozilla.com>
2015-09-21 17:59:26 +0100

    Update client.py to pull from mozilla-release by default a=releasemerge ba=releasemerge CLOSED TREE

--- client.py
+++ client.py
@@ -26,7 +26,7 @@ DEFAULTS = {
       'INSPECTOR_REV':  'DOMI_2_0_16',
 
     # URL of the default hg repository to clone for Mozilla.
-    'MOZILLA_REPO': 'https://hg.mozilla.org/releases/mozilla-beta/',
+    'MOZILLA_REPO': 'https://hg.mozilla.org/releases/mozilla-release/',
 }
 
 REPO_SHORT_NAMES = {
