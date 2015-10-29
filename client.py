b00fedcc81455d05db4df0bcf46f1f95b0e78a62
Mark Banner <standard8@mozilla.com>
2015-10-29 19:27:02 +0000

    Version bumps for SeaMonkey and Thunderbird a=betamerge,ba=betamerge CLOSED TREE

--- client.py
+++ client.py
@@ -26,7 +26,7 @@ DEFAULTS = {
       'INSPECTOR_REV':  'DOMI_2_0_16',
 
     # URL of the default hg repository to clone for Mozilla.
-    'MOZILLA_REPO': 'https://hg.mozilla.org/releases/mozilla-aurora/',
+    'MOZILLA_REPO': 'https://hg.mozilla.org/releases/mozilla-beta/',
 }
 
 REPO_SHORT_NAMES = {
