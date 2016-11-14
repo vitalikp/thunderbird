5e343c203a6440a9c747a8189644d15cdb661f69
Patrick Cloke <clokep@gmail.com>
2016-11-14 12:48:14 -0500

    Version bumps for SeaMonkey and Thunderbird a=auroramerge,ba=auroramerge CLOSED TREE
    a=release to get past UUID checks.

--- client.py
+++ client.py
@@ -17,16 +17,16 @@ DEFAULTS = {
 
     # URL of the default hg repository to clone for ChatZilla.
     'CHATZILLA_REPO': 'https://hg.mozilla.org/chatzilla/',
-    # The stable revision to use for the next branch
-    #  'CHATZILLA_REV':  'SEA2_42_RELBRANCH',
+    # The stable revision to use
+      'CHATZILLA_REV':  'SEA2_42_RELBRANCH',
 
     # URL of the default hg repository to clone for DOM Inspector.
     'INSPECTOR_REPO': 'https://hg.mozilla.org/dom-inspector/',
-    # The stable revision to use for the next branch
-    #  'INSPECTOR_REV':  'DOMI_2_0_16_GECKO_45',
+    # The stable revision to use
+      'INSPECTOR_REV':  'DOMI_2_0_16_GECKO_45',
 
     # URL of the default hg repository to clone for Mozilla.
-    'MOZILLA_REPO': 'https://hg.mozilla.org/mozilla-central/',
+    'MOZILLA_REPO': 'https://hg.mozilla.org/releases/mozilla-aurora/',
 }
 
 REPO_SHORT_NAMES = {
