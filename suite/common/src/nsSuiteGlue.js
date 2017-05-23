e31c02291dccee10f2ac8cc70d6131841f4145c0
rsx11m <rsx11m.pub@gmail.com>
2017-05-23 11:07:46 -0500

    Bug 1366593 - Don't run old migration code every time at startup, only when suite.migration.version doesn't exist yet. r=frg, a=IanN

--- suite/common/src/nsSuiteGlue.js
+++ suite/common/src/nsSuiteGlue.js
@@ -354,8 +354,6 @@ SuiteGlue.prototype = {
   _onProfileStartup: function()
   {
     this._migrateUI();
-    this._updatePrefs();
-    this._migrateDownloadPrefs();
     migrateMailnews(); // mailnewsMigrator.js
 
     Sanitizer.checkSettings();
@@ -379,7 +377,7 @@ SuiteGlue.prototype = {
   {
     const UI_VERSION = 2;
 
-    // If the pref is not set this is a new or pre SeaMonkey 2.50 profile.
+    // If the pref is not set this is a new or pre SeaMonkey 2.49 profile.
     // We can't tell so we just run migration with version 0.
     let currentUIVersion = 0;
 
@@ -390,9 +388,13 @@ SuiteGlue.prototype = {
     if (currentUIVersion >= UI_VERSION)
       return;
 
-    // Migrate remote content exceptions for email addresses which are
-    // encoded as chrome URIs.
     if (currentUIVersion < 1) {
+      // Run any migrations due prior to 2.49.
+      this._updatePrefs();
+      this._migrateDownloadPrefs();
+
+      // Migrate remote content exceptions for email addresses which are
+      // encoded as chrome URIs.
       let permissionsDB =
         Services.dirsvc.get("ProfD", Components.interfaces.nsILocalFile);
       permissionsDB.append("permissions.sqlite");
